f877adbb31170339a115bbba578bd6fc5fad4cc8
devsupa <paulrosset96@gmail.com>
2017-04-19 23:38:41 +0200

    Adding folder docker v1

---
+++ Docker/image_node/config.js
@@ -0,0 +1,14 @@
+module.exports = {
+    user: "Paul Rosset",
+    apiKeysWU: "771533f0d5509845",
+    location: {
+        languages: "fr", // fr and en supported atm
+        country: "France",
+        city: "Nice"
+    },
+    configTheme: {
+        author: "",
+        nameTheme: "default"
+        // @todo Customize theme
+    }
+}
